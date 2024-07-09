cd7ee2f281d754f455376d8807ad6e5759cbaeb0
MatthewDo <matthewdoarda@gmail.com>
2024-07-10 03:02:40 +0800

    added uploaded file handling

--- index.php
+++ index.php
@@ -15,6 +15,31 @@
   
    }
 
+  if(isset($_POST['image_submit'])) {
+    $allowedExt = array('img','png','jpg');
+      if(!empty($_FILES['image'])){
+         $file_name = $_FILES['image']['name'];
+         $file_size = $_FILES['image']['size'];
+         $file_tmp =  $_FILES['image']['tmp_name'];
+         $target_directory = "./uploads/$file_name";
+          //check file extension
+          $file_name = explode('.', $file_name);
+          $file_name = strtolower(end($file_name));
+          if(in_array($file_name,$allowedExt)){
+             if($file_size <= 10000000){
+                 move_uploaded_file($file_tmp,$target_directory);
+                 $message =  "<h3 style='color:green;'>Successfully uploaded</h3>";
+             }
+             else{
+               $message =   "<h3 style='color:red;'>File exceed to 10MB</h3>";
+             }
+          }
+         else{
+          $message = "<h3 style='color:red;'>File not allowed</h3>";
+         }
+      } 
+  }
+
  ?>
 
 <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="POST"> 
@@ -31,3 +56,13 @@
      <a href = '/PHPpractice/dashboard.php'>GO TO DASHBOARD</a>
      </div>
  </form>
+
+   <?php echo $message ?? null  ?>
+
+ <form action = "<?php echo $_SERVER['PHP_SELF'];?>" method="POST" enctype="multipart/form-data">
+      <div>
+        <div>Select Image to Upload</div>
+        <div><input type="file" name="image"> </div>
+        <input type="submit" value="Upload Image" name="image_submit">
+      </div>
+  </form>
